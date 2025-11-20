decadc46db5699cae48b67ffb828e17a6140dfef
rak104 <rak104@mail.aub.edu>
2025-11-20 19:05:06 +0200

    Add basic unit tests for greet

--- tests/test_app.py
+++ tests/test_app.py
@@ -13,5 +13,5 @@ class TestApp(unittest.TestCase):
 	def test_greet(self):
 		self.assertEqual(greet("World"), "Hello, World from Rawad Kansoh!")
 
-if name == " main ":
+if __name__ == "__main__ ":
     unittest.main()
